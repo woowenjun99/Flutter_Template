10b3affd3e02872ff81fa6d9ffc785281f326152
woowenjun99 <woowenjun99@gmail.com>
2024-06-19 23:26:44 +0800

    Add in Firebase

---
+++ lib/src/routing/app_router.dart
@@ -0,0 +1,11 @@
+import 'package:go_router/go_router.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'app_router.g.dart';
+
+@riverpod
+GoRouter appRouter(ref) {
+  return GoRouter(
+    routes: [],
+  );
+}
